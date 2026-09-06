253f6bf0d7029a2a1e0784978fcf398da02b92cd
Preetham goud <preethamgoud2006@gmail.com>
2026-09-06 18:36:13 +0530

    Time: 0 ms (100%), Space: 42.6 MB (28.76%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,36 +1,29 @@
 class Solution {
-
     public List<List<Integer>> combinationSum3(int k, int n) {
-
+        
         List<List<Integer>> ans = new ArrayList<>();
 
-        solve(1, k, n, new ArrayList<>(), ans);
+        solve(1 , k , n , ans , new ArrayList<>(),0);
 
         return ans;
     }
 
-    private void solve(int start,
-                       int k,
-                       int target,
-                       List<Integer> path,
-                       List<List<Integer>> ans) {
+    private static void solve(int start , int range , int target , List<List<Integer>> ans , List<Integer> path,int sum){
 
-        if (path.size() == k) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(path));
-            }
-            return;
+        if(path.size() == range){
+                if(sum == target){
+                    ans.add(new ArrayList<>(path));
+                }
         }
 
-        for (int i = start; i <= 9; i++) {
+        for(int i=start;i<=9;i++){
 
-            if (i > target) break; // pruning
+        path.add(i);
+        
+        solve(i+1, range,target,ans,path,sum+i);
 
-            path.add(i);
-
-            solve(i + 1, k, target - i, path, ans);
-
-            path.remove(path.size() - 1);
+        path.remove(path.size() - 1);
         }
+
     }
 }
